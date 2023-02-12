0517e070e3dffa35f37907b6f8ca9143b5e8362b
shimhyunbo <qushe8r@gmail.com>
2023-02-13 01:12:08 +0900

    2) 정적 자원 관리 - WebIgnore 설정

---
+++ src/main/java/me/qushe8r/studyspringsecurity5_7/security/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package me.qushe8r.studyspringsecurity5_7.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager users() {      // 테스트용 유저
+        String password = passwordEncoder().encode("1111");    // 비밀번호 인코딩
+
+        UserDetails user = User.builder()    // user
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+
+        UserDetails manager = User.builder()  // manager
+                .username("manager")
+                .password(password)
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()   // admin
+                .username("admin")
+                .password(password)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorization -> authorization
+                        .antMatchers("/").permitAll()
+                        .antMatchers("/mypage").hasRole("USER")
+                        .antMatchers("/message").hasRole("MANAGER")
+                        .antMatchers("/config").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                );
+        http
+                .formLogin();
+
+        return http.build();
+    }
+
+}
