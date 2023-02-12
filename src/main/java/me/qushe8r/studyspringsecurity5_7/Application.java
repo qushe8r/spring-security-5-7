8a9bfdbe759ea54de3ab4bb1db8fccfc2702f6ce
shimhyunbo <qushe8r@gmail.com>
2023-02-13 00:37:48 +0900

    1) 실전 프로젝트 생성

---
+++ src/main/java/me/qushe8r/studyspringsecurity5_7/Application.java
@@ -0,0 +1,13 @@
+package me.qushe8r.studyspringsecurity5_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
