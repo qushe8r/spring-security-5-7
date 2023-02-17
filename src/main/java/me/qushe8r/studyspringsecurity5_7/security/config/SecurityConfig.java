4f77f71ed05041553f24aa113858e243065ab577
shimhyunbo <qushe8r@gmail.com>
2023-02-18 01:29:12 +0900

    3) 인증 처리자: AjaxAuthenticationProvider

--- src/main/java/me/qushe8r/studyspringsecurity5_7/security/config/SecurityConfig.java
+++ src/main/java/me/qushe8r/studyspringsecurity5_7/security/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package me.qushe8r.studyspringsecurity5_7.security.config;
 
 import lombok.RequiredArgsConstructor;
+import me.qushe8r.studyspringsecurity5_7.security.filter.AjaxLoginProcessingFilter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -14,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -38,6 +42,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = ajaxLoginProcessingFilter(authenticationManager);
+
         http
                 .authorizeHttpRequests(authorization -> authorization
                         .antMatchers("/", "/users", "/login/**", "/login/*").permitAll()
@@ -58,8 +65,23 @@ public class SecurityConfig {
         http
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler);
+        http
+                .csrf().disable();
+        http
+                .addFilterBefore(ajaxLoginProcessingFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    @Bean
+    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter(AuthenticationManager authenticationManager) {
+        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManager);
+        return ajaxLoginProcessingFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
 }
---
+++ src/main/java/me/qushe8r/studyspringsecurity5_7/security/filter/AjaxLoginProcessingFilter.java
@@ -0,0 +1,47 @@
+package me.qushe8r.studyspringsecurity5_7.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.qushe8r.studyspringsecurity5_7.domain.AccountDto;
+import me.qushe8r.studyspringsecurity5_7.security.token.AjaxAuthenticationToken;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public AjaxLoginProcessingFilter() {
+        super(new AntPathRequestMatcher("/api/login"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (!isAjax(request)) {
+            throw new IllegalStateException("Authentication is not supported!");
+        }
+
+        AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
+        if (!StringUtils.hasText(accountDto.getUsername()) || !StringUtils.hasText(accountDto.getPassword())) {
+            throw new InsufficientAuthenticationException("Username or Password is empty!");
+        }
+
+        AjaxAuthenticationToken ajaxAuthenticationToken = AjaxAuthenticationToken.unauthenticated(accountDto.getUsername(),
+                accountDto.getPassword());
+        return getAuthenticationManager().authenticate(ajaxAuthenticationToken);
+    }
+
+    private boolean isAjax(HttpServletRequest request) {
+        if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+            return true;
+        }
+        return false;
+    }
+}
